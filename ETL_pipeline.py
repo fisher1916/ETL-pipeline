0e3571d618084bb2d076ff0554193f3604ef2c85
Jay Hastings <jayhjman@gmail.com>
2020-12-13 12:36:43 -0800

    added cms processing and did some print cleanup

--- ETL_pipeline.py
+++ ETL_pipeline.py
@@ -1,6 +1,6 @@
 # Import packages that do the specific tasks
 import fips_state_codes as fsc
-
+import clean_cms as cc
 #
 # Main processing pipeline function
 #
@@ -8,11 +8,15 @@ import fips_state_codes as fsc
 
 def process_pipleine():
 
-    print("Starting ETL processing...")
+    print(">>> Starting ETL processing... <<<")
 
+    # Extract and clean fips state codes
     fsc.extract_codes()
 
-    print("Completed ETL processing...")
+    # Read and Clean CMS Data
+    cc.scrub_cms_data()
+
+    print(">>> Completed ETL processing... <<<")
 
 
 process_pipleine()
